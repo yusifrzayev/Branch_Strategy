19532745ddfcf3886a35b59a90cba3f688a42db7
yusifrzayev <josephrzayev@gmail.com>
2023-01-03 15:37:21 -0500

    Updated water.java

--- src/Water.java
+++ src/Water.java
@@ -3,5 +3,7 @@ public class Water {
     public static void main(String[] args) {
 
         System.out.println("Do you want a glass of water?");
+
+        System.out.println("Yes, please");
     }
 }
