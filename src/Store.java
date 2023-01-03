5872e6d4a4b2e4e6d68fc7e7d7ccd7fe5180b439
yusifrzayev <josephrzayev@gmail.com>
2023-01-03 15:48:16 -0500

    Updated Store.java

--- src/Store.java
+++ src/Store.java
@@ -5,5 +5,7 @@ public class Store {
         System.out.println("Welcome to Joseph's Store!");
 
         System.out.println("You can buy a hat, a bottle of water and different food");
+
+        System.out.println("Do you have store card?");
     }
 }
